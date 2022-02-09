edd08e370704a31c17ae896d896b05c8f2fa726c
Jesus Rosales <jrosales6213@gmail.com>
2022-02-08 21:55:37 -0800

    searchbar

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const { google } = require("googleapis");
+const cors = require('cors');
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 // app.get("/", (req, res) => {
 //   res.render("index");
 // });
 
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+app.get("/" , function (req , res) {
+  res.sendFile(__dirname + "/index.html");
+})
+
 app.get("/", async (req, res) => {
  
-
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
@@ -33,16 +41,18 @@ app.get("/", async (req, res) => {
   });
 
   // Write row(s) to spreadsheet
-//   await googleSheets.spreadsheets.values.append({
-//     auth,
-//     spreadsheetId,
-//     range: "Sheet1!A:B",
-//     valueInputOption: "USER_ENTERED",
-//     resource: {
-//       values: [[request, name]],
-//     },
-//   });
-  res.send(console.log(getRows.data.values));
+  // await googleSheets.spreadsheets.values.append({
+  //   auth,
+  //   spreadsheetId,
+  //   range: "Sheet1!A:B",
+  //   valueInputOption: "USER_ENTERED",
+  //   resource: {
+  //     values: [[request, date]],
+  //   },
+  // });
+  const newData = getRows.data
+
+  res.send(newData);
 });
 
 app.listen(3000, (req, res) => console.log("running on 3000"));
---
+++ src/app.js
@@ -0,0 +1,90 @@
+const para = document.querySelector(".paragraph");
+const buttton = document.querySelector(".search-button");
+const container = document.querySelector(".container");
+const table = document.querySelector(".table");
+// const symbol = document.querySelector(".symbol")
+const searchBar = document.getElementById('searchbar');
+
+
+const api_url = "https://api2.binance.com/api/v3/ticker/24hr";
+  
+const info = [];
+// Defining async function to retrieve data from api_url
+async function fetchData(url) {
+    const response = await fetch(url);
+
+    // Storing data in form of JSON
+    var data = await response.json();
+    // console.log(data);
+    if (response) {
+      console.log("we got a response");
+     
+    }
+    show(data);
+    search(data);
+   
+}
+// Calling that async function
+fetchData(api_url);
+  
+
+// Function to define innerHTML for HTML table
+function show(data) {
+    let contentTable = 
+        `
+        <thread>
+        <tr>
+          <th scope="col" >Symbol</th>
+          <th scope="col">Last Price</th>
+          <th scope="col">Open Price</th>
+          <th scope="col">Volume</th>
+         </tr>
+         </thread>
+         `;
+
+    // Loop to access all rows 
+    for (let r of data) {
+        contentTable += `
+        
+        <tbody class="table-body">
+        <tr> 
+
+    <td >${r.symbol} </td>
+    <td >${r.lastPrice} </td>
+    <td >${r.openPrice} </td>
+    <td >${r.volume} </td>
+ 
+           
+</tr>
+</tbody>
+`;
+    }
+    // Setting innerHTML as tab variable
+    table.innerHTML = contentTable;
+}
+
+function displayCharacters(filtered){
+if (filtered){
+  
+}
+
+}
+
+
+
+function search(d){
+// event listener inside function
+    searchBar.addEventListener("keyup", e => {
+          const searchString = e.target.value.toUpperCase();
+          const filteredCharacters = d.filter(character => {
+            return (
+              character.symbol.includes(searchString) 
+            );
+          });
+          // displayCharacters(filteredCharacters);
+          console.log(filteredCharacters);
+        });
+    
+}
+
+
