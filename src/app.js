fd3973cf3e83e975e187a4fd001b0129df71982d
Jesus Rosales <jrosales6213@gmail.com>
2022-02-09 06:36:11 -0800

    deleted non esential files

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const container = document.querySelector(".container");
 const table = document.querySelector(".table");
 // const symbol = document.querySelector(".symbol")
 const searchBar = document.getElementById('searchbar');
+const tableRow = document.querySelector('.table-row');
 
 
 const api_url = "https://api2.binance.com/api/v3/ticker/24hr";
@@ -12,18 +13,16 @@ const info = [];
 // Defining async function to retrieve data from api_url
 async function fetchData(url) {
     const response = await fetch(url);
-
     // Storing data in form of JSON
     var data = await response.json();
     // console.log(data);
     if (response) {
       console.log("we got a response");
-     
     }
     show(data);
     search(data);
-   
 }
+
 // Calling that async function
 fetchData(api_url);
   
@@ -34,7 +33,7 @@ function show(data) {
         `
         <thread>
         <tr>
-          <th scope="col" >Symbol</th>
+          <th scope="col">Symbol</th>
           <th scope="col">Last Price</th>
           <th scope="col">Open Price</th>
           <th scope="col">Volume</th>
@@ -45,35 +44,21 @@ function show(data) {
     // Loop to access all rows 
     for (let r of data) {
         contentTable += `
-        
         <tbody class="table-body">
-        <tr> 
-
+        <tr class="table-row"> 
     <td >${r.symbol} </td>
     <td >${r.lastPrice} </td>
     <td >${r.openPrice} </td>
-    <td >${r.volume} </td>
- 
-           
+    <td >${r.volume} </td>          
 </tr>
 </tbody>
 `;
     }
-    // Setting innerHTML as tab variable
+ // Setting innerHTML as tab variable
     table.innerHTML = contentTable;
 }
 
-function displayCharacters(filtered){
-if (filtered){
-  
-}
-
-}
-
-
-
 function search(d){
-// event listener inside function
     searchBar.addEventListener("keyup", e => {
           const searchString = e.target.value.toUpperCase();
           const filteredCharacters = d.filter(character => {
@@ -81,10 +66,8 @@ function search(d){
               character.symbol.includes(searchString) 
             );
           });
-          // displayCharacters(filteredCharacters);
-          console.log(filteredCharacters);
+          show(filteredCharacters);
         });
-    
 }
 
 
